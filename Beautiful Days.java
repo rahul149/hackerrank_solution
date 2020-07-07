d42d2b98cf9e99c981290e85dc14f20ccfa918cc
Rahul <53679844+rahul149@users.noreply.github.com>
2020-07-07 15:13:53 +0530

    Create Beautiful Days.java
    Given a range of numbered days[i,...j], and a number k , determine the number of days in the range that are beautiful. Beautiful numbers are defined as numbers where
     |i-reverse(i)| is evenly divisible by  k. If a day's value is a beautiful number, it is a beautiful day. Print the number of beautiful days in the range.

---
+++ Beautiful Days.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the beautifulDays function below.
+    static int beautifulDays(int x, int j, int k) {
+        int b=0;
+        for(int i=x;i<=j;i++){
+        int n=i,reverse=0;
+            while(n != 0)
+        { reverse = reverse * 10;
+         reverse = reverse + n%10;
+          n = n/10; }
+        if((Math.abs(i-reverse))%k==0)
+        b++;
+        }
+        return b;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] ijk = scanner.nextLine().split(" ");
+
+        int i = Integer.parseInt(ijk[0]);
+
+        int j = Integer.parseInt(ijk[1]);
+
+        int k = Integer.parseInt(ijk[2]);
+
+        int result = beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
